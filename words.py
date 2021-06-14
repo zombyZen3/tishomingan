f03ac9d6a15b7a4930f08192a9b3a0529dcf325f
zombyZen3 <82547284+zombyZen3@users.noreply.github.com>
2021-06-14 17:50:28 -0400

    Update words.py

--- words.py
+++ words.py
@@ -1,88 +1,177 @@
 # If ypu are looking through this file then I'm so sad since I had to type out curse words :(((
+
 # I'm crying :(((((
+
+
 
 wtt = {
 
+
+
 'feminine': 'fweminit',
+
 'english': 'ing-wish',
+
 '100': 'a-hedrid',
+
 'full': 'fwore',
+
 '3': 'fwee',
+
 'three': 'fwee',
+
 '4': 'fwore',
+
 'four': 'fwore',
+
 'no': 'just no',
+
 'yes': 'it means yes',
+
 'right': 'white',
+
 'tablet': 'tab-l *throughs up* let',
+
 'call': 'c *distorted yell* all',
+
 'called': 'c *distorted yell* alled',
+
 'caller': 'c *distorted yell* aller',
+
 'stop': 'STWAAAAAPP',
+
 'bless': 'brush',
+
 'people': 'no its people',
+
 'because': 'ba-cus',
+
 'phone': 'pwone',
+
 'phones': 'pwones',
+
 'telephones': 'telepwones',
+
 'telephone': 'telepwone',
+
 'google': 'is hey google',
+
 'tishomingo': 'tishamingo',
+
 'computer': 'compwutur',
+
 'what': 'wghaaaat',
+
 'simulator': 'simuwator',
+
 'clone': 'chrome book',
+
 'cat': 'dog',
+
 'dog': 'cat',
+
 'mouth': 'malf',
+
 'the': 'da',
+
 'ha': 'eh',
 
 
 
+
+
+
+
 'fuck': 'sun',
+
 'fucker': 'sunner',
+
 'motherfucker': 'mothersunner',
+
 'god': 'sun',
+
 'bullshit': '*BEEEP*',
+
 'shit': '*BEEEP*',
+
 'bitch': '*BEEEP*',
+
 'prick': '*BEEEP*',
+
 'pussy': '*BEEEP*',
+
 'cock': '*BEEEP*',
+
 'dickhead': '*BEEEP*',
+
 'dick': '*BEEEP*',
 
 
 
+
+
+
+
 'steal': 'stue',
+
 'chromebook': 'google doc bookshelf status bar',
+
 'shelf': 'bookshelf',
+
 'doc': 'google doc',
+
 'jayce': 'Jacob',
+
 'really': 'welly',
+
 'zomby': 'zombay',
+
 'queen': 'QueeeeenB',
+
 'queenb': 'QueeeeenB',
+
 'brother': 'bruder',
+
 'brothers': 'bruders',
+
 'rename': 'wuname',
+
 'vip': '2 times',
+
 'crash': 'quosh',
+
 'brother': 'butter bottle',
+
 'bald': 'baald',
+
 'crazy': 'in a cartoon wu-wurld',
+
 'world': 'wu-wurld',
+
 'screen': 'scween',
+
 'Edward': 'Edwoord',
+
 'game': 'day',
+
 'games': 'IT\'S GAMES QUEENB',
+
 'quiet': 'hey hey hey hey hey',
+
 'froza': 'Fworza',
+
 'horizon': 'Howizon',
+
 'red': 'greeen',
+
 'green': 'red',
+
 'sweaty': 'sweety'
+
+'barn': 'bagel'
 }
 
+
+
 list = wtt.keys()
+
